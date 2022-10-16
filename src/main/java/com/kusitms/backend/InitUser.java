8efb2684c355bda92ff8a8c169ab2c1835919f15
dldmsql <yhy5913@gmail.com>
2022-10-16 15:06:17 +0900

    chore: Set dummy data that is user for easy to test

---
+++ src/main/java/com/kusitms/backend/InitUser.java
@@ -0,0 +1,40 @@
+package com.kusitms.backend;
+
+import com.kusitms.backend.domain.User;
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class InitUser {
+
+  private final InitUserService initUserService;
+
+  @PostConstruct
+  public void init() {
+    initUserService.init();
+  }
+
+  @Component
+  @RequiredArgsConstructor
+  static class InitUserService {
+    private final EntityManager entityManager;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public void init() {
+      User user = User.builder()
+          .email("test@test.com")
+          .password(passwordEncoder.encode("test12"))
+          .contact("010-0000-0000")
+          .address("서울시 마포구")
+          .nickname("Dual")
+          .build();
+      entityManager.persist(user);
+    }
+  }
+}
