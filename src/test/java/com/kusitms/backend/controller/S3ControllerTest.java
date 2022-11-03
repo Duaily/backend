6768e2f11d31f18702f546104d5ffb5fab16e186
dldmsql <yhy5913@gmail.com>
2022-11-04 01:13:56 +0900

    test: Create aws s3 image upload and delete API docs file

---
+++ src/test/java/com/kusitms/backend/controller/S3ControllerTest.java
@@ -0,0 +1,148 @@
+package com.kusitms.backend.controller;
+
+import static com.kusitms.backend.ApiDocumentUtils.getDocumentRequest;
+import static com.kusitms.backend.ApiDocumentUtils.getDocumentResponse;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestPartBody;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kusitms.backend.config.JwtAccessDeniedHandler;
+import com.kusitms.backend.config.JwtAuthenticationEntryPoint;
+import com.kusitms.backend.config.JwtSecurityConfig;
+import com.kusitms.backend.config.TokenProvider;
+import com.kusitms.backend.service.IS3Service;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+@ActiveProfiles("test")
+@ExtendWith(RestDocumentationExtension.class)
+@WebMvcTest(S3Controller.class)
+@MockBean(JpaMetamodelMappingContext.class)
+class S3ControllerTest {
+
+  @Autowired
+  MockMvc mockMvc;
+  @MockBean
+  TokenProvider tokenProvider;
+  @Autowired
+  ObjectMapper objectMapper;
+  @MockBean
+  JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+  @MockBean
+  JwtAccessDeniedHandler jwtAccessDeniedHandler;
+  @MockBean
+  JwtSecurityConfig jwtSecurityConfig;
+  @MockBean
+  IS3Service s3Service;
+  @MockBean
+  AmazonS3Client amazonS3Client;
+  @MockBean
+  private Authentication authentication;
+  @MockBean
+  private SecurityContext securityContext;
+
+  @BeforeEach
+  void setUp(WebApplicationContext webApplicationContext,
+      RestDocumentationContextProvider restDocumentationContextProvider) {
+    mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+        .addFilters(new CharacterEncodingFilter("UTF-8", true))
+        .apply(documentationConfiguration(restDocumentationContextProvider)
+            .operationPreprocessors()
+            .withRequestDefaults(prettyPrint())
+            .withResponseDefaults(prettyPrint()))
+        .build();
+  }
+
+  @Test
+  void upload() throws Exception {
+    MockMultipartFile multipartFile = new MockMultipartFile(
+        "multipartFile",
+        "multipartFile.jpeg",
+        MediaType.IMAGE_JPEG_VALUE,
+        "multipartFile".getBytes()
+    );
+
+    String imageUrl = "aws-s3-bucket/test_db?imageUrl";
+
+    given(s3Service.upload(multipartFile)).willReturn(imageUrl);
+
+    ResultActions result = mockMvc.perform(
+        RestDocumentationRequestBuilders
+            .multipart("/api/s3")
+            .file("file", multipartFile.getBytes())
+            .contentType(MediaType.MULTIPART_FORM_DATA)
+            .accept(MediaType.APPLICATION_JSON)
+    );
+
+    result.andExpect(status().isOk())
+        .andDo(print())
+        .andDo(
+            document("aws-s3-upload", getDocumentRequest(), getDocumentResponse(),
+                requestPartBody("file"),
+                responseFields(
+                    fieldWithPath("status").description("결과 코드"),
+                    fieldWithPath("message").description("응답 메세지"),
+                    fieldWithPath("data").description("업로드 이미지 주소")
+                ))
+        );
+  }
+
+  @Test
+  void delete() throws Exception {
+
+    String fileName = anyString();
+
+    s3Service.delete(fileName);
+
+    ResultActions resultActions = mockMvc.perform(
+        RestDocumentationRequestBuilders
+            .delete("/api/s3?fileName={fileName}", fileName)
+            .characterEncoding("utf-8")
+            .accept(MediaType.APPLICATION_JSON)
+    );
+
+    resultActions.andExpect(status().isOk())
+        .andDo(print())
+        .andDo(
+            document("aws-s3-delete", getDocumentRequest(), getDocumentResponse(),
+                requestParameters(
+                    parameterWithName("fileName").description("이미지 url")
+                ),
+                responseFields(
+                    fieldWithPath("status").description("결과 코드"),
+                    fieldWithPath("message").description("응답 메세지"),
+                    fieldWithPath("data").description("응답 데이터 없음")
+                )
+            )
+        );
+  }
+}
