f843d62e25cc544b9887f534152e7bcb8a7da886
dldmsql <yhy5913@gmail.com>
2022-11-04 18:31:21 +0900

    feat: Create deal API

--- src/main/java/com/kusitms/backend/domain/Deal.java
+++ src/main/java/com/kusitms/backend/domain/Deal.java
@@ -2,13 +2,19 @@ package com.kusitms.backend.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Getter
 @Entity
 public class Deal extends MetaEntity {
@@ -18,10 +24,20 @@ public class Deal extends MetaEntity {
   @Column(name = "deal_id")
   private Long id;
 
+  @Enumerated(EnumType.STRING)
+  private DealState state;
+
   @OneToOne
   private User buyer;
 
   @OneToOne
   private Post housePost;
 
+  public static Deal createDeal(User buyer, Post housePost) {
+    return Deal.builder()
+        .buyer(buyer)
+        .housePost(housePost)
+        .state(DealState.ONGOING)
+        .build();
+  }
 }
--- src/main/java/com/kusitms/backend/service/IHouseService.java
+++ src/main/java/com/kusitms/backend/service/IHouseService.java
@@ -1,8 +1,11 @@
 package com.kusitms.backend.service;
 
+import com.kusitms.backend.dto.DealDto;
 import com.kusitms.backend.dto.HouseDto;
 
 public interface IHouseService {
 
   Long create(String email, HouseDto dto);
+
+  Long createDeal(DealDto.Request request, String email);
 }
