441444a007159964131035289fc4b0374425b656
dldmsql <yhy5913@gmail.com>
2022-11-07 23:39:04 +0900

    chore: Separate local and dev environment to enhance develop

--- src/main/java/com/kusitms/backend/service/AuthService.java
+++ src/main/java/com/kusitms/backend/service/AuthService.java
@@ -13,6 +13,7 @@ import com.kusitms.backend.dto.TokenRequestDto;
 import com.kusitms.backend.exception.ApiException;
 import com.kusitms.backend.exception.ApiExceptionEnum;
 import com.kusitms.backend.repository.UserRepository;
+import com.kusitms.backend.util.OauthClient;
 import com.kusitms.backend.util.RedisClient;
 import java.util.Optional;
 import javax.transaction.Transactional;
@@ -42,9 +43,9 @@ public class AuthService implements IAuthService {
   private final PasswordEncoder passwordEncoder;
   private final RedisClient redisClient;
   private final UserRepository userRepository;
+  private final OauthClient oauthClient;
 
-  @Value("${spring.security.oauth2.client.registration.kakao.client-id}")
-  private String clientId;
+
 
   @Transactional
   public TokenDto signIn(SignInRequest request) {
@@ -115,96 +116,28 @@ public class AuthService implements IAuthService {
   @Transactional
   public TokenDto kakaoSignIn(String code) throws JsonProcessingException {
     // (1) 인가코드로 AccessToken 요청
-    String accessToken = getAccessToken(code);
-
+    String accessToken = oauthClient.getAccessToken(code);
+    log.debug("access token form kakao : " + accessToken);
     // (2) 토큰으로 카카오 API 호출 - 유저 정보 조회
-    AuthDto.Request authDto = getKakaoUserInfo(accessToken);
-
+    AuthDto.Request authDto = oauthClient.getProfile(accessToken);
+    log.debug("authDto from kakao : " + authDto.getEmail());
     // (3) 카카오 계정으로 회원가입 처리
-    User user = signUpKakao(authDto);
-
+    User user = oauthClient.signUp(authDto);
+    log.debug("user id : " + user.getId());
     // (4) 로그인 처리
     UsernamePasswordAuthenticationToken authenticationToken
         = new UsernamePasswordAuthenticationToken(
-        user.getEmail(), ""); // id/pw 기반으로 authenticationToken 생성
+        user.getEmail(), ""); // email 기반으로 authenticationToken 생성
     Authentication authentication = managerBuilder.getObject().authenticate(authenticationToken);
 
     // (5) 토큰 DTO 생성
-    return tokenProvider.generateTokenDto(authentication);
-  }
+    TokenDto tokenDto =  tokenProvider.generateTokenDto(authentication);
 
-  private User signUpKakao(AuthDto.Request authDto) {
-
-    if (userRepository.existsByEmail(authDto.getEmail())) {
-      throw new ApiException(ApiExceptionEnum.AUTH_DUPLICATED_EXCEPTION);
-    }
-
-    return userRepository.save(User.builder()
-        .email(authDto.getEmail())
-        .nickname(authDto.getNickname())
-        .authority(Authority.ROLE_USER)
-        .build());
-  }
-
-  private AuthDto.Request getKakaoUserInfo(String accessToken) throws JsonProcessingException {
-
-    HttpHeaders headers = new HttpHeaders();
-    headers.add("Authorization", "Bearer " + accessToken);
-    headers.add("Content-type",
-        "application/x-www-form-urlencoded;charset=utf-8");
-
-    HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(headers);
-    RestTemplate restTemplate = new RestTemplate();
-    ResponseEntity<String> response = restTemplate.exchange(
-        "https://kapi.kakao.com/v2/user/me",
-        HttpMethod.POST,
-        request,
-        String.class
-    );
-
-    String responseBody = response.getBody();
-    ObjectMapper objectMapper = new ObjectMapper();
-    JsonNode jsonNode = objectMapper.readTree(responseBody);
-
-    Long id = jsonNode.get("id").asLong();
-    String email = jsonNode.get("kakao_account").get("email").asText();
-    String nickname = jsonNode.get("properties").get("nickname").asText();
-
-    AuthDto.Request authDto = AuthDto.Request.builder().build();
-    authDto.setNickname(nickname);
-    authDto.setEmail(email);
-
-    return authDto;
-  }
-
-  private String getAccessToken(String code) throws JsonProcessingException {
-    // (1) Header 생성
-    HttpHeaders headers = new HttpHeaders();
-    headers.add("Content-Type",
-        "application/x-www-form-urlencoded;charset=utf-8");
-
-    // (2) Param 생성
-    MultiValueMap<String, String> param = new LinkedMultiValueMap<>();
-    param.add("grant_type", "authorization_code");
-    param.add("client_id", clientId);
-    param.add("redirect_uri", "http://localhost:8080/api/auth/kakao");
-    param.add("code", code);
-
-    // (3) 요청 보내기
-    HttpEntity<MultiValueMap<String, String>> kakaoRequest = new HttpEntity<>(param, headers);
-    RestTemplate restTemplate = new RestTemplate();
-    ResponseEntity<String> responseEntity = restTemplate.exchange(
-        "https://kauth.kakao.com/oauth/token",
-        HttpMethod.POST,
-        kakaoRequest,
-        String.class
-    );
-
-    // (4) 응답 파싱
-    String responseBody = responseEntity.getBody();
-    ObjectMapper objectMapper = new ObjectMapper();
-    JsonNode jsonNode = objectMapper.readTree(responseBody);
-    return jsonNode.get("access_token").asText();
+    // (6) refresh token 레디스 저장
+    redisClient.setValue(authentication.getName(), tokenDto.getRefreshToken(),
+        tokenProvider.getRefreshTokenExpireTime());
+    log.debug("token : " + tokenDto.getAccessToken());
+    return tokenDto;
   }
 
   //닉네임 중복 체크
