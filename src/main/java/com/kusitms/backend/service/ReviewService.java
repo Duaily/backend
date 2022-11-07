cf1974d826399b269a29af01335762a8db644ea6
JAEHEEE <y2hjjh@naver.com>
2022-11-07 12:41:11 +0900

    feat: Get Review Post list API

--- src/main/java/com/kusitms/backend/service/ReviewService.java
+++ src/main/java/com/kusitms/backend/service/ReviewService.java
@@ -15,12 +15,8 @@ import java.util.List;
 import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.stereotype.Service;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 
 @Service
 @RequiredArgsConstructor
