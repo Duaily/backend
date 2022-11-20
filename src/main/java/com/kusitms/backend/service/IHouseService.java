049999f85d8267d04d7e15993938e9ffcecfe690
JAEHEEE <y2hjjh@naver.com>
2022-11-20 18:26:06 +0900

    feat: Create get user information API

--- src/main/java/com/kusitms/backend/service/IHouseService.java
+++ src/main/java/com/kusitms/backend/service/IHouseService.java
@@ -4,7 +4,6 @@ import com.kusitms.backend.domain.HousePost;
 import com.kusitms.backend.dto.DealDto;
 import com.kusitms.backend.dto.HouseDto;
 import com.kusitms.backend.dto.HousePreviewDto;
-import com.kusitms.backend.dto.UserDto;
 import com.kusitms.backend.response.PageResponse;
 import java.util.List;
 import org.springframework.data.domain.PageRequest;
--- src/main/java/com/kusitms/backend/service/IUserService.java
+++ src/main/java/com/kusitms/backend/service/IUserService.java
@@ -1,6 +1,7 @@
 package com.kusitms.backend.service;
 
 import com.kusitms.backend.dto.CheckSmsRequest;
+import com.kusitms.backend.dto.UserDto;
 
 public interface IUserService {
 
