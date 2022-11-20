f22a34d24d8c1d647b16fcdd08d7dc8497f01dd4
dldmsql <yhy5913@gmail.com>
2022-11-20 15:10:07 +0900

    chore: AOP logging for req and res and during time

--- src/main/java/com/kusitms/backend/BackendApplication.java
+++ src/main/java/com/kusitms/backend/BackendApplication.java
@@ -2,8 +2,10 @@ package com.kusitms.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableAspectJAutoProxy
 @EnableJpaAuditing
 @SpringBootApplication
 public class BackendApplication {
